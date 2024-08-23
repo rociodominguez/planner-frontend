6ba4444d1769452d3919744aba1bb024381cc2a1
Rocío Domínguez <rociodominguezcontacto@gmail.com>
2024-08-23 13:28:07 +0200

    componetización

--- src/components/ProfileComponent.js
+++ src/components/ProfileComponent.js
@@ -1,5 +1,5 @@
-import { handleUpdateProfile } from './ErrorComponent.js';
 import { API_URL } from '../services/ApiService.js';
+import { renderEvents } from './EventsComponent.js';
 
 export const renderProfile = async () => {
     const contentDiv = document.getElementById('content');
@@ -40,4 +40,30 @@ export const renderProfile = async () => {
     }
 };
 
+export const handleUpdateProfile = async (e) => {
+    e.preventDefault();
+    const newUsername = document.getElementById('newUsername').value;
 
+    const token = localStorage.getItem('token');
+    try {
+        const response = await fetch(`${API_URL}/users/profile`, {
+            method: 'PUT',
+            headers: { 
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ userName: newUsername })
+        });
+
+        if (response.ok) {
+            localStorage.setItem('username', newUsername);
+            renderEvents();
+
+            renderProfile();
+        } else {
+            console.log('Error al actualizar el nombre de usuario:', await response.text());
+        }
+    } catch (error) {
+        console.log('Error en la solicitud de actualización de perfil:', error);
+    }
+};
